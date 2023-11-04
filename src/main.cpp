8ff5fbb26e8c1851bccd7b6819329cbe428cc3c4
Letícia S. Lenzoni <125830543+leticiascofield@users.noreply.github.com>
2023-11-03 21:10:33 -0300

    BubbleSort, SelectionSort e InsertionSort feitos

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,65 @@ class Vertice{
     }
 };
 
+class Sort{
+    public:
+
+    void Troca(Vertice*& a, Vertice*& b){
+        Vertice* aux = a;
+        a = b;
+        b = aux;
+    }
+
+    void Estabilizar(Vertice* vertices[], int n){
+       for(int i = 0; i < n-1; i++){
+            for(int j = 1; j < n-i; j++){
+                if(vertices[j]->cor == vertices[j-1]->cor){
+                    if(vertices[j]->id > vertices[j-1]->id){
+                        Troca(vertices[j], vertices[j-1]);
+                    }
+                }
+            }
+       }
+    }
+
+    void BubbleSort(Vertice* vertices[], int n){
+        for(int i = 0; i < n-1; i++){
+            for(int j = 1; j < n-i; j++){
+                if(vertices[j]->cor < vertices[j-1]->cor){
+                    Troca(vertices[j], vertices[j-1]);
+                }
+            }
+        }
+    }
+
+    void SelectionSort(Vertice* vertices[], int n){
+        int min;
+        for(int i = 0; i < n-1; i++){
+            min = i;
+            for(int j = i+1; j < n; j++){
+                if(vertices[j]-> cor < vertices[min]->cor){
+                    min = j;
+                }
+            }
+            Troca(vertices[i], vertices[min]);
+        }
+        Estabilizar(vertices, n);
+    }
+
+    void InsertionSort(Vertice* vertices[], int n){
+        Vertice* aux;
+        for(int i = 1; i < n; i++){
+            aux = vertices[i];
+            int j = i - 1;
+            while((j >= 0) && (aux->cor < vertices[j]->cor)){
+                vertices[j+1] = vertices[j];
+                j--;
+            }
+            vertices[j+1] = aux;
+        }
+    }
+};
+
 bool Guloso(Vertice* vertices[], int n) {
     for(int k = 0; k < n; k++){ //passar por todos os vertices e testar se é guloso
         bool caso[vertices[k]->cor-1] = {false};
@@ -45,7 +104,7 @@ int main (){
     char c;
     int n;
 
-    //cin >> c;
+    cin >> c;
     cin >> n;
 
     Vertice* vertices[n];
@@ -74,7 +133,28 @@ int main (){
         cout << endl;
     }
 
-    cout << "É guloso? " << Guloso(vertices, n) << endl;
+    Sort sort;
+    cout << Guloso(vertices, n) << " ";
+    if(Guloso(vertices, n)) {
+        switch(c) {
+            case 'b':
+                sort.BubbleSort(vertices, n);
+                break;
+            case 's':
+                sort.SelectionSort(vertices, n);
+                break;
+            case 'i':
+                sort.InsertionSort(vertices, n);
+                break;
+            default:
+                cout << "Erro: comando inválido." << endl;
+        }
+        
+        for(int i = 0; i < n; i++){
+            cout << vertices[i]->id << " ";
+        }
+    }
+    cout << endl;
 
     for(int i = 0; i < n; i++){
         delete vertices[i];
